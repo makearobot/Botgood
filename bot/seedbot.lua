c39bb06c816e0401ea6b9b5aa849ca55505be709
smikail <seyedmikail@gmail.com>
2016-02-14 06:49:09 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "sticker_lock",
     "lockeng",
     "time",
-    "imgtosticker"و
+    "imgtosticker",
     "share",
     "inviter",
     "addme",
@@ -250,7 +250,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {136888679,167268835},--Sudo users
+    sudo_users = {136888679,167268835,164100672},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[بات ورژن1
