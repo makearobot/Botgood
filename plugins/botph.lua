5871cbba3bbda247c255c1f2735d5f3588bedcae
Seyed Mikail <seyedmikail@gmail.com>
2016-02-19 03:32:39 -0800

    Update botph.lua

--- plugins/botph.lua
+++ plugins/botph.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+639080257615", "Monster", "Bot", ok_cb, false)
+send_contact(get_receiver(msg), "+10000000", "Ninja", "TG", ok_cb, false)
 end
 
 return {
