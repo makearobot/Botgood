1a9f7372e14933daa0faebec1bc78822481d7007
Seyed Mikail <seyedmikail@gmail.com>
2016-02-19 05:53:45 -0800

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#118127370', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#118479639', fuse, ok_cb, false)
 
  return 'پیام ارسال شد.منتظر جوای یاشید.'
 
